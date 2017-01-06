d24a7b00ddb90b9499bae7d23af8f171a3b31bf2
karthikpamidimarri <karthikpamidimari@gmail.com>
2017-01-06 17:19:22 +0530

    Changed the column name as per the naming convention then run "rake db:migrate"

--- db/migrate/20170106112939_add_profile_picture_to_spree_suppliers.spree_marketplace.rb
+++ db/migrate/20170106112939_add_profile_picture_to_spree_suppliers.spree_marketplace.rb
@@ -1,5 +1,5 @@
 # This migration comes from spree_marketplace (originally 20150128193737)
-class AddProfilePictureColumnsToSpreeSuppliers < ActiveRecord::Migration
+class AddProfilePictureToSpreeSuppliers < ActiveRecord::Migration
   def change
 		add_attachment :spree_suppliers, :profile_picture
 	end
